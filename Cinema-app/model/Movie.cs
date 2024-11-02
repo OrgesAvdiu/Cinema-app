e162cbe688ceab57ccc13651b608b99f7931e8ef
Orgesss <orgesiav@gmail.com>
2024-11-03 00:55:16 +0100

    Database Connect

---
+++ Cinema-app/model/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinema_app.model
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+
+    }   
+}
--- Cinema-app/model/dbconnect.cs
+++ Cinema-app/model/dbconnect.cs
@@ -1,6 +1,23 @@
-﻿namespace Cinema_app.model
+﻿using System;
+using MySql.Data.MySqlClient;
+using Microsoft.EntityFrameworkCore;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+using Cinema_app.model;
+namespace MSSQLApp.Data { 
+
+
+
+public class dbconnect : DbContext                                                                         
 {
-    public class dbconnect
+    public IConfiguration _config { get; set; }
+    public dbconnect(IConfiguration config)
     {
+        _config = config;
     }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_config.GetConnectionString("DatabaseConnection"));
+    }
+    public DbSet<Movie> Movies { get; set; }    
+}
 }

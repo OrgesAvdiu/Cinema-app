f4ba021de09533f60f05a2c4130a9a9bc4d5f4cc
lummeta25 <lummeta25@gmail.com>
2024-11-05 13:58:32 +0100

    Cinemas, Rooms CRUD operations in backend

---
+++ Cinema-app/model/Cinema.cs
@@ -0,0 +1,11 @@
+﻿namespace Cinema_app.model
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public List<Room> Rooms { get; set; } = new List<Room>();
+        public string ContactInfo { get; set; }
+    }
+}
---
+++ Cinema-app/model/Room.cs
@@ -0,0 +1,11 @@
+﻿namespace Cinema_app.model
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public int RoomNumber { get; set; }
+        public int Capacity { get; set; }
+        public int CinemaId { get; set; }
+        public string Features { get; set; }
+    }
+}
